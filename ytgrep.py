7cc794a97db99d3e54fc7160df917df3338a8549
Alex Kohler <alexjohnkohler@gmail.com>
2019-01-20 14:26:12 -0500

    removing some todos

--- ytgrep.py
+++ ytgrep.py
@@ -3,7 +3,6 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser(description='ytgrep')
-# TODO wire up to take in url instead of video ID
 parser.add_argument(
     '-e',
     action="store_true",
@@ -14,15 +13,12 @@ parser.add_argument(
     help='Print debug information while searching')
 parser.add_argument('pattern', type=str, help='term to search for')
 parser.add_argument('url', nargs='+', help='video URL')
-#parser.add_argument('searchString', metavar='N', type=int, nargs='+', help='revisions')
 
-# don't allow url and regex to be set at same time
 args = parser.parse_args()
 args_dict = vars(args)
 
 # TODO case insensitivity?
-# TODO validate URL passed in
-# TODO multiple videos?
+# TOOD unit tests
 
 video_id = args.url
 download = Download(args_dict)
